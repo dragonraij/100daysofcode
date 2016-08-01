93ca0290c4fb9f5ca3abc8b8afb46b86e3a674ca
Raj Prasad <rajeshwarrprasad@yahoo.com>
2016-08-01 07:23:08 +0000

    update bottom caption with text input

--- jquery/01Bonsai/app.js
+++ jquery/01Bonsai/app.js
@@ -7,3 +7,7 @@ $(document).ready(main);
 $( "#top-text" ).keyup(function() {
   $( ".top-caption" ).text($("#top-text").val());
 });
+
+$( "#bottom-text" ).keyup(function() {
+  $( ".bottom-caption" ).text($("#bottom-text").val());
+});

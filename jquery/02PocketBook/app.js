a924557ed3f144ed4eb58c6984a1c0a8dd54c6e2
Raj Prasad <rajeshwarrprasad@yahoo.com>
2016-08-02 08:04:13 +0000

    add custom error messages on form submit

--- jquery/02PocketBook/app.js
+++ jquery/02PocketBook/app.js
@@ -3,7 +3,32 @@ var main = function() {
     var firstName = $('#first').val();
     
     if(firstName === "") {
-      
+      $('.first-name-error').text("Please Enter your first name")
+    }
+    
+    var lastName = $('#last').val();
+    
+    if(lastName === "") {
+      $('.last-name-error').text("Please Enter your last name")
+    }
+    
+    var email = $('#email').val();
+    
+    if(email === "") {
+      $('.email-error').text("Please Enter your email")
+    }
+    
+    var password = $('#password').val();
+    
+    if(password === "") {
+      $('.password-error').text("Please Enter a password")
+    }
+    else if (password.length < 8){
+        $('.password-error').text("Good passwords should be longer than 8 characters")
+    }
+    
+    if (email ==="admin@pocketbook.com") {
+      $('.email-error').text("That email is already taken")        
     }
 
     return false;

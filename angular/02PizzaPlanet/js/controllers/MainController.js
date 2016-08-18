d313e9ef3b75999a48d19d56d709947b1f78c3a7
Raj Prasad <rajeshwarrprasad@yahoo.com>
2016-08-18 05:03:20 +0000

    add bruschetta to menu

--- angular/02PizzaPlanet/js/controllers/MainController.js
+++ angular/02PizzaPlanet/js/controllers/MainController.js
@@ -11,6 +11,29 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
+    }
+  ];
+  
+    $scope.mains = [
+    {
+      name: 'Margherita',
+      description: 'Marinara sauce, mozzarella, fresh basil.',
+      price: 6.95
+    },
+    {
+      name: 'Pepperoni',
+      description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano.',
+      price: 7.95
+    },
+    {
+      name: 'Rustica',
+      description: 'Marinara sauce, kalamata olives, roasted garlic, fresh oregano.',
+      price: 8.95
     }
   ];
 
